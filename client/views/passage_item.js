b1e333b2a99774e419db1b743516bbc300048837
Caroline Meeks <caroline@meekshome.com>
2014-02-04 21:25:13 -0500

    updates the database with the word#

--- client/views/passage_item.js
+++ client/views/passage_item.js
@@ -4,14 +4,20 @@ Template.passageItem.helpers({
 Template.passageItem.rendered = function() {
     Meteor.defer(function() {
 	$('.passage-content').lettering('words');
-
-	$("div.passage-content > span" ).mousedown(function() {
-	    re = new RegExp("word([0-9]*)")
-	    classes = $(this).attr("class");
-            word_num = re.exec( $(this).attr("class"))[1];
-	    $(this).addClass('highlight');
-	    console.log(classes + ' regex got  ' + word_num);
-	});
     });
 };
+
+
+Template.passageItem.events({
+    'click div.passage-content': function(e) {
+//	    re = new RegExp("word([0-9]*)")
+	    re = new RegExp("word[0-9]*")
+            word_class = re.exec( $(e.target).attr("class"))[0];
+	    $(e.target).addClass('highlight');
+	    passageId = this._id;
+	    console.log(' regex got  ' + word_class + ' passageId ' + passageId);
+	    Meteor.call('highlight', word_class, passageId);
+    }
+});
+
 
--- collections/passages.js
+++ collections/passages.js
@@ -1,1 +1,14 @@
 Passages = new Meteor.Collection('passages');
+
+Meteor.methods({
+    highlight: function(word_class, passageId) {
+	var passage = Passages.findOne(passageId);
+
+	if (!passage)
+	    throw new Meteor.Error(422, 'Post not found');
+
+	Passages.update(passage._id, {
+	    $addToSet: {wordHighlights: word_class}
+	});
+    }
+});

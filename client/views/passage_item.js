1f535f2c143332f3dd4461461c70a3ca104450e8
Caroline Meeks <caroline@meekshome.com>
2014-02-04 20:07:32 -0500

    highlighter shows up as cursor. Word Number in consoler

--- client/views/passage_item.js
+++ client/views/passage_item.js
@@ -3,8 +3,15 @@ Template.passageItem.helpers({
 
 Template.passageItem.rendered = function() {
     Meteor.defer(function() {
-	console.log('hi console');
 	$('.passage-content').lettering('words');
+
+	$("div.passage-content > span" ).mousedown(function() {
+	    re = new RegExp("word([0-9]*)")
+	    classes = $(this).attr("class");
+            word_num = re.exec( $(this).attr("class"))[1];
+	    $(this).addClass('highlight');
+	    console.log(classes + ' regex got  ' + word_num);
+	});
     });
 };
 

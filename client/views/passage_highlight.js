fbf4115eff34d20e14be7c338f1b8655f06c795e
Caroline Meeks <caroline@meekshome.com>
2014-02-05 16:03:57 -0500

    Added passage_highlight
    Before used passage_item for both but we only want highlight on the
    one_passage view not on the list view.

---
+++ client/views/passage_highlight.js
@@ -0,0 +1,40 @@
+Template.passageHighlight.helpers({
+
+});
+
+Template.passageHighlight.rendered = function() {
+    Meteor.defer(function() {
+	$('.passage-content').lettering('words');
+	var passageId = $('.one-passage').attr('id');
+	data = Passages.findOne(passageId);
+	wordHighlights = data.wordHighlights;
+	wordHighlights.forEach(function(wordClass) {
+	    $('.' + wordClass).addClass('highlight');
+	});
+	console.log(wordHighlights);
+    });
+};
+
+
+Template.passageHighlight.events({
+    'click div.passage-content': function(e) {
+	re = new RegExp("word[0-9]*")
+	passageId = this._id;
+	var classes =  $(e.target).attr("class");
+	if (classes != 'passage-content') { //FIXMEsometimes the click doesn't seem to get a span
+	    console.log(classes)
+            word_class = re.exec(classes)[0];
+	    console.log(' regex got  ' + word_class + ' passageId ' + passageId);
+
+	    if ($(e.target).hasClass('highlight')) {
+		$(e.target).removeClass('highlight');
+		Meteor.call('highlight', word_class, passageId, 'remove');
+	    } else {
+		$(e.target).addClass('highlight');
+		Meteor.call('highlight', word_class, passageId, 'add');
+	    };
+	};
+    }
+});
+
+

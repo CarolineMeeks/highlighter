de42ac91ccb4f3b685053206aa58e7f037e815d6
Caroline Meeks <caroline@meekshome.com>
2014-02-05 14:09:40 -0500

    Add and delete highlighting

--- client/views/passage_item.js
+++ client/views/passage_item.js
@@ -1,22 +1,35 @@
 Template.passageItem.helpers({
+
 });
 
 Template.passageItem.rendered = function() {
     Meteor.defer(function() {
 	$('.passage-content').lettering('words');
+	var passageId = $('.one-passage').attr('id');
+	data = Passages.findOne(passageId);
+	wordHighlights = data.wordHighlights;
+	wordHighlights.forEach(function(wordClass) {
+	    $('.' + wordClass).addClass('highlight');
+	});
+	console.log(wordHighlights);
     });
 };
 
 
 Template.passageItem.events({
     'click div.passage-content': function(e) {
-//	    re = new RegExp("word([0-9]*)")
-	    re = new RegExp("word[0-9]*")
-            word_class = re.exec( $(e.target).attr("class"))[0];
+	re = new RegExp("word[0-9]*")
+	passageId = this._id;
+        word_class = re.exec( $(e.target).attr("class"))[0];
+	console.log(' regex got  ' + word_class + ' passageId ' + passageId);
+
+	if ($(e.target).hasClass('highlight')) {
+	    $(e.target).removeClass('highlight');
+	    Meteor.call('highlight', word_class, passageId, 'remove');
+	} else {
 	    $(e.target).addClass('highlight');
-	    passageId = this._id;
-	    console.log(' regex got  ' + word_class + ' passageId ' + passageId);
-	    Meteor.call('highlight', word_class, passageId);
+	    Meteor.call('highlight', word_class, passageId, 'add');
+	};
     }
 });
 
--- collections/passages.js
+++ collections/passages.js
@@ -1,14 +1,23 @@
 Passages = new Meteor.Collection('passages');
 
 Meteor.methods({
-    highlight: function(word_class, passageId) {
+    highlight: function(word_class, passageId, action) {
 	var passage = Passages.findOne(passageId);
-
+	
 	if (!passage)
 	    throw new Meteor.Error(422, 'Post not found');
-
-	Passages.update(passage._id, {
-	    $addToSet: {wordHighlights: word_class}
-	});
+  
+	if (action = 'add') {
+	    
+	    Passages.update(passage._id, {
+		$addToSet: {wordHighlights: word_class}
+	    });
+	} else if (action = 'remove') {
+	    Passages.update(passage._id, {
+		$pull:  {wordHighlights: word_class}
+	    });
+	} else {
+	    throw new Meteor.Error(422, 'action not add or remove');
+	};    
     }
 });

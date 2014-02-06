e288c1e77b0a5d5495623accc29b6403fc162003
Caroline Meeks <caroline@meekshome.com>
2014-02-06 11:26:21 -0500

    Bug that kept unhighlight from being recorded

--- collections/passages.js
+++ collections/passages.js
@@ -4,20 +4,24 @@ Meteor.methods({
     highlight: function(word_class, passageId, action) {
 	var passage = Passages.findOne(passageId);
 	
-	if (!passage)
+	if (!passage) {
 	    throw new Meteor.Error(422, 'Post not found');
-  
-	if (action = 'add') {
-	    
-	    Passages.update(passage._id, {
-		$addToSet: {wordHighlights: word_class}
-	    });
-	} else if (action = 'remove') {
-	    Passages.update(passage._id, {
-		$pull:  {wordHighlights: word_class}
-	    });
 	} else {
-	    throw new Meteor.Error(422, 'action not add or remove');
-	};    
+
+	    console.log(action + word_class);
+	    if (action == 'add') {
+		console.log('about to addToSet');
+		Passages.update(passage._id, {
+		    $addToSet: {wordHighlights: word_class}
+		});
+	    } else if (action == 'remove') {
+		console.log('about to remove');
+		Passages.update(passage._id, {
+		    $pullAll:  {wordHighlights: [word_class]}
+		});
+	    } else {
+		throw new Meteor.Error(422, 'action not add or remove');
+	    };    
+	};
     }
 });

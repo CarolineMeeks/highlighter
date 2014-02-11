9c22aedf74fcb73f254a713f6035cfa9610bdec5
Caroline Meeks <caroline@meekshome.com>
2014-02-11 18:26:55 -0500

    Use D3 on heat map

--- client/views/passage_heat_map.js
+++ client/views/passage_heat_map.js
@@ -5,15 +5,26 @@ Template.passageHeatMap.helpers({
 Template.passageHeatMap.rendered = function() {
     console.log('in HeatMap');
     var passageId = this.data._id;
-   
+
     Meteor.defer(function() {
 	$('.passage-content').lettering('words');
 	var passage = Passages.findOne(passageId);
-	_(passage).each( function(value, key, passage) {
-	    console.log(key + value);
-	    if (value >= 1) {
-		$('.' + key).addClass('highlight');
-	    };
+	var freq = []
+	$('.passage-content').find("span").each( function(index) {
+	    word = 'word'+(index+1);
+	    var count = passage[word];
+	    if (!count) { count = 0 }
+	    freq.push(count)
+	    if (count >= 1) {
+		d3.select('.'+word).transition().delay(500).style('background-color','yellow');
+		if (count >= 2) {
+		    d3.select('.'+word).transition().delay(1000).style('color','red');
+		    if (count >= 3) {
+			d3.select('.'+word).transition().delay(1500).style('font-size','150%');
+		    }		
+		}
+	    }
+	    console.log('looping through a span..' + word + $(this).text() + count);
 	});
     });
 };
